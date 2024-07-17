bb6fff94b2c92190b439d8f7bea01f8506549ba4
somethingxdev <hevnxv19@gmail.com>
2024-07-17 21:37:32 +0200

    remove scrollTrigger

--- src/components/home/Map.tsx
+++ src/components/home/Map.tsx
@@ -12,63 +12,33 @@ const items = [
 
 export function Map({ countriesData }) {
   const [currentState, setCurrentState] = useState('countries');
-  const sectionRef = useRef(null);
   const progressBarRef = useRef(null);
   const maxBarHeight = 375;
-  const intervalDuration = 5;
 
-  useEffect(() => {
-    let currentIndex = 0;
+  const handleStateChange = (newState) => {
+    const newIndex = items.findIndex((item) => item.title === newState);
 
-    const updateProgressBar = () => {
-      const nextIndex = (currentIndex + 1) % items.length;
-      const nextItem = items[nextIndex];
-
-      const isReturningToStart = currentIndex === items.length - 1;
-
-      if (window.innerWidth > 768) {
-        gsap.to(progressBarRef.current, {
-          height: isReturningToStart ? `${maxBarHeight}px` : `${(nextIndex / (items.length - 1)) * maxBarHeight}px`,
-          width: '3px',
-          duration: intervalDuration,
-          ease: 'linear',
-          onComplete: () => {
-            if (isReturningToStart) {
-              gsap.set(progressBarRef.current, { height: 0 });
-            }
-          }
-        });
-      } else {
-        gsap.to(progressBarRef.current, {
-          width: isReturningToStart ? '100%' : `${nextItem.progress}%`,
-          height: '1px',
-          duration: intervalDuration,
-          ease: 'linear',
-          onComplete: () => {
-            if (isReturningToStart) {
-              gsap.set(progressBarRef.current, { width: 0 });
-            }
-          }
-        });
-      }
-
-      gsap.delayedCall(intervalDuration, () => {
-        setCurrentState(nextItem.title);
-        currentIndex = nextIndex;
+    if (window.innerWidth > 768) {
+      gsap.to(progressBarRef.current, {
+        height: `${(newIndex / (items.length - 1)) * maxBarHeight}px`,
+        width: '3px',
+        duration: 0.5,
+        ease: 'power2.inOut',
       });
-    };
+    } else {
+      gsap.to(progressBarRef.current, {
+        width: `${items[newIndex].progress}%`,
+        height: '3px',
+        duration: 0.5,
+        ease: 'power2.inOut',
+      });
+    }
 
-    const interval = setInterval(updateProgressBar, intervalDuration * 1000);
-    updateProgressBar(); // Start the first iteration immediately
-
-    return () => {
-      clearInterval(interval);
-      gsap.killTweensOf(progressBarRef.current);
-    };
-  }, []);
+    setCurrentState(newState);
+  };
 
   return (
-    <section className="map container pt-20 md:pt-[120px]" id="eastern" ref={sectionRef}>
+    <section className="map container pt-20 md:pt-[120px]" id="eastern">
       <div className="relative mb-10 flex h-[900px] w-full flex-col md:h-[950px] lg:flex-row xl:h-auto">
         <div className="z-20 max-w-4xl">
           <div className="mb-10">
@@ -83,15 +53,16 @@ export function Map({ countriesData }) {
             </a>
           </div>
           <div className="relative z-20">
-            <div className="absolute left-0 top-0 h-[1px] w-full bg-gray-300 lg:top-12 lg:h-[375px] lg:w-[3px]"></div>
-            <div className="absolute left-0 top-0 h-[1px] overflow-x-hidden bg-black lg:top-12 lg:h-0 lg:w-[3px]" ref={progressBarRef}></div>
+            <div className="absolute left-0 top-0 h-[3px] w-full bg-gray-300 lg:top-12 lg:h-[375px] lg:w-[3px]"></div>
+            <div className="absolute left-0 top-0 h-[3px] overflow-x-hidden bg-black lg:top-12 lg:h-0 lg:w-[3px]" ref={progressBarRef}></div>
             <ol className="relative flex justify-between lg:z-0 lg:flex-col lg:gap-10 lg:pl-14">
               {items.map((item, index) => (
                 <li
                   key={index}
-                  className={`relative z-20 w-fit pt-10 transition-colors after:absolute after:-top-[7px] after:size-4 after:-translate-x-1/2 after:rounded-full last:after:left-full lg:pt-0 lg:after:-left-[55px]  lg:after:-top-[5px] lg:first:after:top-10 lg:last:after:-left-[55px] lg:last:after:top-10 [&:nth-child(2)]:after:left-1/2 lg:[&:nth-child(2)]:after:-left-[55px]  lg:[&:nth-child(2)]:after:top-12 ${
+                  className={`relative z-20 w-fit cursor-pointer pt-10 transition-colors after:absolute after:-top-[7px] after:size-4 after:-translate-x-1/2 after:rounded-full last:after:left-full lg:pt-0 lg:after:-left-[55px] lg:after:-top-[5px] lg:first:after:top-10 lg:last:after:-left-[55px] lg:last:after:top-10 [&:nth-child(2)]:after:left-1/2 lg:[&:nth-child(2)]:after:-left-[55px] lg:[&:nth-child(2)]:after:top-12 ${
                     currentState === item.title ? 'text-black after:bg-black' : 'text-gray-1 after:bg-gray-1'
-                  }`}>
+                  }`}
+                  onClick={() => handleStateChange(item.title)}>
                   <div className="flex flex-col gap-2">
                     <span className="lg:text-25 text-[40px] font-medium leading-none text-current sm:text-2xl md:text-3xl md:font-normal lg:text-[100px] lg:leading-[110px]">{item.count}+</span>
                     <span className="text-sm capitalize text-current md:text-lg">{item.title}</span>
