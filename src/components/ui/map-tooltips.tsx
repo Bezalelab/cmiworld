fd1a14a5c8f66999d86d186da656503e32e82712
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2024-07-20 12:31:44 +0200

    update map

--- src/components/ui/map-tooltips.tsx
+++ src/components/ui/map-tooltips.tsx
@@ -1,19 +1,25 @@
-import { useState, useEffect, useRef, useMemo } from 'react';
+import { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { useDebounce } from '@uidotdev/usehooks';
 import { CircleFlag } from 'react-circle-flags';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { useInView } from 'react-intersection-observer';
 
-const MapTooltips = ({ items, currentState }) => {
-  const [openStates, setOpenStates] = useState(() => {
-    const initialState = Array(items.length).fill(false);
-    initialState[0] = true;
-    return initialState;
+function preloadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = resolve;
+    img.onerror = reject;
+    img.src = src;
   });
-  const [shownTooltips, setShownTooltips] = useState([0]);
+}
+
+const MapTooltips = ({ items, currentState }) => {
+  const [openStates, setOpenStates] = useState(() => new Array(items.length).fill(false));
+  const [shownTooltips, setShownTooltips] = useState([]);
   const [isHoveringMap, setIsHoveringMap] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
-  const debouncedOpenStates = useDebounce(openStates, 200);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+  const debouncedOpenStates = useDebounce(openStates, 500);
   const intervalRef = useRef(null);
 
   const { ref: intersectionRef, inView } = useInView({
@@ -29,9 +35,17 @@ const MapTooltips = ({ items, currentState }) => {
   }, []);
 
   useEffect(() => {
+    const imagesToPreload = ['/churches.svg', '/churches-tooltip.svg'];
+    Promise.all(imagesToPreload.map(preloadImage))
+      .then(() => setImagesLoaded(true))
+      .catch((err) => console.error('Error preloading images', err));
+  }, []);
+
+  useEffect(() => {
     if (!isHoveringMap && inView) {
       intervalRef.current = setInterval(() => {
-        const availableIndices = items.map((_, index) => index).filter((index) => !shownTooltips.includes(index));
+        const availableIndices = items.reduce((acc, _, index) => (!shownTooltips.includes(index) ? [...acc, index] : acc), []);
+
         if (availableIndices.length > 0) {
           const randomIndex = availableIndices[Math.floor(Math.random() * availableIndices.length)];
           setOpenStates((prevStates) => prevStates.map((_, i) => i === randomIndex));
@@ -41,54 +55,76 @@ const MapTooltips = ({ items, currentState }) => {
     } else {
       clearInterval(intervalRef.current);
     }
+
     return () => clearInterval(intervalRef.current);
   }, [items.length, shownTooltips, isHoveringMap, inView]);
 
-  const handleMouseEnter = (index) => !isMobile && setOpenStates((prevStates) => prevStates.map((_, i) => i === index));
-  const handleMouseLeave = () => !isMobile && setOpenStates((prevStates) => prevStates.map(() => false));
-  const handleClick = (index) => isMobile && setOpenStates((prevStates) => prevStates.map((state, i) => (i === index ? !state : false)));
-
-  const renderTooltip = useMemo(
-    () => (item, idx) => {
-      const isChurches = currentState === 'churches';
-      const triggerContent = isChurches ? (
-        <img src="/churches.svg" className="h-[22px]" alt="Church" />
-      ) : (
-        <CircleFlag countryCode={item.city} width="22" className="z-10 h-[22px] rounded-full border-2 lg:pointer-events-none" />
-      );
-
-      const popoverContent = (
-        <a href={`/countries/${item.slug}/`} className="flex items-center gap-2">
-          {isChurches ? (
-            <img src="/churches-tooltip.svg" className="size-9" alt="Church tooltip" />
-          ) : (
-            <CircleFlag countryCode={item.city} className="z-10 rounded-full lg:pointer-events-none" width="36" />
-          )}
-          <div className="flex flex-col gap-0.5">
-            <span className="text-xs font-bold text-black">{item.name}</span>
-          </div>
-        </a>
-      );
-
-      return (
-        <div key={idx} className="group absolute" style={{ right: `${item.right}%`, top: `${item.top}%` }}>
-          <Popover open={debouncedOpenStates[idx]} onOpenChange={() => {}}>
-            <PopoverTrigger className="relative h-[22px] outline-none" onMouseEnter={() => handleMouseEnter(idx)} onMouseLeave={() => handleMouseLeave()} onClick={() => handleClick(idx)}>
-              {triggerContent}
-            </PopoverTrigger>
-            <PopoverContent className="absolute !-left-6 !-top-10 pr-3" onMouseEnter={() => handleMouseEnter(idx)} onMouseLeave={() => handleMouseLeave()}>
-              {popoverContent}
-            </PopoverContent>
-          </Popover>
-        </div>
-      );
+  const handleMouseEnter = useCallback(
+    (index) => {
+      if (!isMobile) {
+        setOpenStates((prevStates) => prevStates.map((_, i) => i === index));
+      }
     },
-    [currentState, debouncedOpenStates, isMobile],
+    [isMobile],
   );
+
+  const handleMouseLeave = useCallback(() => {
+    if (!isMobile) {
+      setOpenStates((prevStates) => prevStates.map(() => false));
+    }
+  }, [isMobile]);
+
+  const handleClick = useCallback(
+    (index) => {
+      if (isMobile) {
+        setOpenStates((prevStates) => prevStates.map((state, i) => (i === index ? !state : false)));
+      }
+    },
+    [isMobile],
+  );
+
+  const renderItem = useMemo(
+    () => (item, idx) => (
+      <div
+        key={idx}
+        className="group absolute"
+        style={{
+          right: `${item.right}%`,
+          top: `${item.top}%`,
+        }}>
+        <Popover open={debouncedOpenStates[idx]} onOpenChange={() => {}}>
+          <PopoverTrigger className="relative h-[22px] outline-none" onMouseEnter={() => handleMouseEnter(idx)} onMouseLeave={handleMouseLeave} onClick={() => handleClick(idx)}>
+            {currentState !== 'churches' ? (
+              <CircleFlag countryCode={item.city} width="22" className="z-10 h-[22px] rounded-full border-2 lg:pointer-events-none" />
+            ) : (
+              <img src="/churches.svg" className="h-[22px]" alt="Church" />
+            )}
+          </PopoverTrigger>
+          <PopoverContent className="absolute !-left-6 !-top-10 pr-3" onMouseEnter={() => handleMouseEnter(idx)} onMouseLeave={handleMouseLeave}>
+            <a href={`/countries/${item.slug}/`} className="flex items-center gap-2">
+              {currentState !== 'churches' ? (
+                <CircleFlag countryCode={item.city} className="z-10 rounded-full lg:pointer-events-none" width="36" />
+              ) : (
+                <img src="/churches-tooltip.svg" className="size-9" alt="Church tooltip" />
+              )}
+              <div className="flex flex-col gap-0.5">
+                <span className="text-xs font-bold text-black">{item.name}</span>
+              </div>
+            </a>
+          </PopoverContent>
+        </Popover>
+      </div>
+    ),
+    [currentState, debouncedOpenStates, handleMouseEnter, handleMouseLeave, handleClick],
+  );
+
+  if (!imagesLoaded) {
+    return <div>Loading...</div>; // или любой другой индикатор загрузки
+  }
 
   return (
     <div ref={intersectionRef} onMouseEnter={() => setIsHoveringMap(true)} onMouseLeave={() => setIsHoveringMap(false)}>
-      {items.map(renderTooltip)}
+      {items.map(renderItem)}
     </div>
   );
 };
