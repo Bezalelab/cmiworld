e1d1ec14ee975eb1e1dae2429f3528db424970d6
somethingxdev <hevnxv19@gmail.com>
2024-07-24 22:55:46 +0200

    fix image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,26 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import matthiesenxyzlace from '@matthiesenxyz/astrolace';
-import sitemap from "@astrojs/sitemap";
-import robots from "astro-robots"; // Add code manually
+import sitemap from '@astrojs/sitemap';
+import robots from 'astro-robots';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cmiworld.org',
   image: {
-    remotePatterns: [{
-      protocol: "https"
-    }]
+    remotePatterns: [
+      {
+        protocol: 'https',
+      },
+    ],
   },
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), icon(), matthiesenxyzlace(), sitemap(), robots()]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+    matthiesenxyzlace(),
+    sitemap(),
+    robots(),
+  ],
 });
